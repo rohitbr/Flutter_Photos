e440b66b70e03604b818087845e4873d677e828b
rohitbr <rbhat@getluma.com>
2023-04-08 16:23:19 -0400

    1. Initial commit to json serialization. 2. Added json serializable package. 3. Added freezed package. 4. Loading the list Photo objects on a GridView using above two libraries. 5. Deserialization logic is there in this repository. That is reading from remote repository and converting that into list of Photo objects.

---
+++ flutter_photos/lib/photo.dart
@@ -0,0 +1,48 @@
+/*
+{
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+  }
+  */
+
+// import 'package:json_annotation/json_annotation.dart';
+// part 'photo.g.dart';
+
+// @JsonSerializable(explicitToJson: true)
+// class Photo {
+//   final int albumId;
+//   final int id;
+//   final String title;
+//   final String thumbnailUrl;
+
+//   Photo({
+//     required this.albumId,
+//     required this.id,
+//     required this.title,
+//     required this.thumbnailUrl,
+//   });
+
+//   factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
+
+//   Map<String, dynamic> toJson() => _$PhotoToJson(this);
+// }
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+part 'photo.freezed.dart';
+part 'photo.g.dart';
+
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required int albumId,
+    required int id,
+    required String title,
+    required String thumbnailUrl,
+  }) = _Photo;
+
+  factory Photo.fromJson(Map<String, Object?> json) => _$PhotoFromJson(json);
+}
